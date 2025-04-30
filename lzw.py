7dfc625e6d5f81652d56e265e194aad16ac92ec1
cefqrn <cefqrn@gmail.com>
2025-04-30 03:44:09 -0400

    added lzw.encode and made lzw.decode take bytes for symmetry

--- lzw.py
+++ lzw.py
@@ -2,8 +2,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-from collections.abc import Iterable, Generator
-from itertools import chain, islice
+from __future__ import annotations
+
+from collections.abc import Iterable, Generator, Sequence
+from dataclasses import dataclass, field
+from itertools import batched, chain, islice
 from enum import Enum, auto
 
 MAX_CODE_SIZE = 12
@@ -19,7 +22,7 @@ type Entry = list[int] | SpecialEntry
 
 def bits_to_int(bits: Iterable[bool]) -> int:
     result = 0
-    for bit in reversed(list(bits)):
+    for bit in bits:
         result = 2*result + bit
 
     return result
@@ -29,13 +32,18 @@ def create_dictionary(minimum_code_size: int) -> list[Entry]:
     return [[x] for x in range(2**minimum_code_size)] + [SpecialEntry.CLEAR, SpecialEntry.END]
 
 
-def decode(minimum_code_size: int, data: Iterable[bool]) -> Generator[int, None, None]:
+def byte_to_bits(byte: int) -> list[bool]:
+    return [(byte >> i) & 1 for i in range(8)]
+
+
+def decode(minimum_code_size: int, data: bytes) -> Generator[int, None, None]:
+    bitstream = chain.from_iterable(map(byte_to_bits, data))
     dictionary = create_dictionary(minimum_code_size)
 
     prev = None
     while True:
         code_size = len(dictionary).bit_length()
-        code_index = bits_to_int(islice(data, code_size))
+        code_index = bits_to_int(reversed(list(islice(bitstream, code_size))))
 
         if code_index < len(dictionary):
             code = dictionary[code_index]
@@ -57,3 +65,56 @@ def decode(minimum_code_size: int, data: Iterable[bool]) -> Generator[int, None,
 
         yield from code
         prev = code
+
+
+@dataclass
+class TrieNode:
+    code: int
+    children: dict[TrieNode] = field(default_factory=dict)
+
+
+def encode_to_codes(dictionary: Sequence[int], data: Iterable[int]) -> Generator[int, None, None]:
+    code_count = len(dictionary)
+    if not code_count or code_count & (code_count - 1):
+        raise ValueError("dictionary length must be power of 2")
+
+    clear_code = code_count
+    end_code = code_count + 1
+
+    code_count += 2
+
+    yield (code_count - 1).bit_length(), clear_code
+
+    root = TrieNode(0)
+    for code, entry in enumerate(dictionary):
+        root.children[entry] = TrieNode(code)
+
+    curr = root
+    for entry in data:
+        if entry in curr.children:
+            curr = curr.children[entry]
+        else:
+            yield (code_count - 1).bit_length(), curr.code
+
+            if (code_count + 1).bit_length() <= MAX_CODE_SIZE:
+                curr.children[entry] = TrieNode(code_count)
+                code_count += 1
+
+            curr = root.children[entry]
+
+    yield (code_count - 1).bit_length(), curr.code
+    yield (code_count - 1).bit_length(), end_code
+
+
+def code_to_bits(code_size: int, x: int) -> list[bool]:
+    return [(x >> i) & 1 for i in range(code_size)][::-1]
+
+
+def encode(dictionary: Sequence[int], data: Iterable[int]) -> Generator(int, None, None):
+    bitstream = chain.from_iterable(reversed(code_to_bits(code_size, code)) for code_size, code in encode_to_codes(dictionary, data))
+    for batch in batched(chain(bitstream, 7*[False]), 8):
+        batch = batch[::-1]
+        if len(batch) < 8:
+            break
+
+        yield bits_to_int(batch)
